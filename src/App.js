faa52376f7ea14c793a48357af68e35213c1242a
jstntrn <justin.tran2290@gmail.com>
2019-01-22 11:40:57 -0700

    moved HashRouter to App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import routes from './routes';
-import { Link } from 'react-router-dom';
+import { Link, HashRouter } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <nav className='nav'>
-          <div>WestSide University</div> 
-          <div className='link-wrap'>
-              <Link to="/" className='links'>Home</Link>
-              <Link to="/about" className='links'>About</Link> 
-          </div>
-        </nav>
-        {routes}
-      </div>
+      <HashRouter>
+        <div>
+          <nav className='nav'>
+            <div>WestSide University</div> 
+            <div className='link-wrap'>
+                <Link to="/" className='links'>Home</Link>
+                <Link to="/about" className='links'>About</Link> 
+            </div>
+          </nav>
+          {routes}
+        </div>
+      </HashRouter>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,4 @@ import './main.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(
-    <HashRouter>
-        <App />
-    </HashRouter>
-    , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
